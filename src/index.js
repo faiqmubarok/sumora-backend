a28f21a06decb1749536a4b15e4b54098dd2ea92
Muhammad Faiq Mubarok <faiqmubarok@gmail.com>
2025-06-12 14:29:33 +0700

    initial project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authController from "./auth/auth.controller.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
+app.use("/api/v1/users", authController);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
