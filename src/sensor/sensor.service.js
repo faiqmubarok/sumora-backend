b3223767df9c5810559c7323d1f009512c3164e2
Irsyam Okta Pratama Riyadi <122722355+irsyamokta@users.noreply.github.com>
2025-06-14 13:09:14 +0700

    Feat(prediction): Tambah Prediksi ML ke Data Sensor (#25)
    * Update : New schema
    
    * Add : New migration
    
    * Update : Sensor controller
    
    * Add : Sensor repository
    
    * Add : Sensor service
    
    * Add : New dependencies
    
    * Del : Log error
    
    * Add : Create prediction function
    
    * Add : Prediction logic

--- src/sensor/sensor.service.js
+++ src/sensor/sensor.service.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as sensorRepository from "./sensor.repository.js";
 
 export const getSensorByFilter = async (deviceId, filter) => {
@@ -22,5 +23,30 @@ export const createSensor = async (data) => {
         sulfate
     }
 
-    return sensorRepository.createSensor(sensorData);
+    const savedSensor = await sensorRepository.createSensor(sensorData);
+
+    try {
+        const response = await axios.post("https://sumora-ml-production.up.railway.app/predict", {
+            Sulfate: parseFloat(sulfate),
+            ph: parseFloat(ph),
+            Chloramines: parseFloat(chloromines),
+            Solids: parseFloat(solids),
+        });
+
+        const predictionResult = response.data?.result;
+
+        await sensorRepository.createPrediction({
+            sensorDataId: savedSensor.id,
+            result: predictionResult,
+        });
+
+        return {
+            ...savedSensor,
+            prediction: predictionResult,
+        };
+
+    } catch (error) {
+        console.error("Gagal memanggil ML API:", error.message);
+        return savedSensor;
+    }
 };
