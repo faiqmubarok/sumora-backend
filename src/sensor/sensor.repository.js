154b37193497be622b28d17a60208ae5a164c4ad
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-06-14 11:46:22 +0700

    Add : Sensor repository

---
+++ src/sensor/sensor.repository.js
@@ -0,0 +1,34 @@
+import prisma from "../config/prisma.js";
+
+export const getSensorDataSummary = async (deviceId) => {
+    const [latest, last] = await Promise.all([
+        prisma.sensorData.findFirst({
+            where: { deviceId },
+            orderBy: { createdAt: "desc" },
+        }),
+        prisma.sensorData.findMany({
+            where: { deviceId },
+            orderBy: { createdAt: "desc" },
+            take: 10,
+        }),
+    ]);
+
+    return { latest, last };
+};
+
+export const getLatestSensor = async (deviceId) => {
+    return prisma.sensorData.findFirst({
+        where: { deviceId },
+        orderBy: { createdAt: "desc" },
+    });
+};
+
+export const getLastSensor = async (deviceId) => {
+    return prisma.sensorData.findMany({
+        where: { deviceId },
+        orderBy: { createdAt: "desc" },
+        take: 10,
+    });
+};
+
+export const createSensor = async (data) => prisma.sensorData.create({ data });
