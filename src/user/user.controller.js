d466097d1364f53dfe0afa140012f7c3dbe49e85
Muhammad Faiq Mubarok <faiqmubarok@gmail.com>
2025-06-14 23:42:06 +0700

    feat(user): implement edit profile with image upload (multer + cloudinary)
    - Added PATCH /user/:id route to handle profile updates
    - Integrated multer for handling multipart/form-data (photo upload)
    - Connected to Cloudinary for image storage and deletion
    - Updated user model to store photo as JSON with url and publicId
    - Applied Yup validation for editable fields (name, email, phone)
    - Sanitized and rejected unknown fields in request payload

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import userService from "./user.service.js";
 import { authenticate } from "../middlewares/authenticate.js";
+import { editUserProfileSchema } from "./user.validation.js";
+import multer from "multer";
 
 const router = Router();
-const { getUserByIdService } = userService;
+const storage = multer.memoryStorage();
+const upload = multer({ storage }).single("photo");
+const { getUserByIdService, updateUserByIdService } = userService;
 
 router.get("/:id", authenticate, async (req, res) => {
   try {
@@ -17,6 +21,36 @@ router.get("/:id", authenticate, async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(400).send({ error: error.message });
+  }
+});
+
+router.patch("/:id", authenticate, upload, async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ error: "User id is required" });
+
+    const validated = await editUserProfileSchema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    const updatedUser = await updateUserByIdService(id, validated, req.file);
+
+    res
+      .status(200)
+      .json({ user: updatedUser, message: "User updated successfully" });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(400).json({
+        message: "Validasi gagal",
+        errors: error.errors,
+      });
+    }
+    console.error(error);
+    res.status(400).json({
+      message: "Terjadi kesalahan",
+      error: error.message,
+    });
   }
 });
 
--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,6 +1,10 @@
 import userRepository from "./user.repository.js";
+import {
+  deleteImageFromCloudinary,
+  uploadImageToCloudinary,
+} from "../utils/cloudinary.js";
 
-const { findUserById } = userRepository;
+const { findUserById, updateUserById, findUserByEmail } = userRepository;
 
 const getUserByIdService = async (id) => {
   const user = await findUserById(id);
@@ -10,4 +14,53 @@ const getUserByIdService = async (id) => {
   return user;
 };
 
-export default { getUserByIdService };
+const updateUserByIdService = async (userId, data, file) => {
+  const existingUser = await findUserById(userId);
+  if (!existingUser) throw new Error("User not found");
+
+  let photo = undefined;
+
+  if (data.email) {
+    const existEmail = await findUserByEmail(data.email);
+
+    if (existEmail) {
+      throw new Error("Email is already registered");
+    }
+  }
+
+  if (file) {
+    try {
+      const existingImageUrl = existingUser.photo;
+
+      if (existingImageUrl) {
+        await deleteImageFromCloudinary(existingImageUrl.publicId);
+      }
+
+      const uploadResult = await uploadImageToCloudinary(file.buffer);
+      photo = {
+        url: uploadResult.secure_url,
+        publicId: uploadResult.public_id,
+      };
+    } catch (error) {
+      console.error("Image upload failed:", error);
+      throw new Error(
+        "Image upload failed: " +
+          (error instanceof Error ? error.message : "Unknown error")
+      );
+    }
+  }
+
+  const updatedData = {
+    ...data,
+  };
+
+  if (photo) {
+    updatedData.photo = photo;
+  }
+
+  const updated = await updateUserById(userId, updatedData);
+
+  return updated;
+};
+
+export default { getUserByIdService, updateUserByIdService };
