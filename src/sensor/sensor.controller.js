0c0dbc50fc9d1807630cce5bd565f629b6ce1cac
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-06-14 11:46:07 +0700

    Update : Sensor controller

--- src/sensor/sensor.controller.js
+++ src/sensor/sensor.controller.js
@@ -1,12 +1,25 @@
 import { Router } from "express";
+import * as sensorService from "./sensor.service.js";
 
 const router = Router();
 
-router.post("/", (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const request = req.body;
-    console.log(request);
-    res.status(201).send({ message: "Sensor registered successfully" });
+    const { deviceId } = req.params;
+    const { filter } = req.query;
+
+    const result = await sensorService.getSensorByFilter(deviceId, filter);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error("Error getting sensor data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const result = await sensorService.createSensor(req.body);
+    res.status(201).send({ result });
   } catch (error) {
     console.error(error);
     res.status(400).send({ error: error.message });
